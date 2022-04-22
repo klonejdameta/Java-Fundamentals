831581151c15cc0d5030bebee717d0e77a08c93c
klonejdameta <klonejdameta98@gmail.com>
2022-04-22 11:12:57 +0200

    Second commit

--- src/puppyfirstprogram/Dhimbo.java
+++
@@ -1,8 +0,0 @@
-package puppyfirstprogram;
-
-public class Dhimbo {
-    public static void main(String[] args) {
-        Puppy puppy = new Puppy("Klonejda", 23);
-        System.out.println(puppy);
-    }
-}
